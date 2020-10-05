cf56598dcafb312b94037fec9e133dbe27001542
Peyton Shetler <peyton.shetler@gmail.com>
2020-10-05 15:03:30 -0500

    Initial Commit

---
+++ CalculatorSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Peyton Shetler on 10/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
